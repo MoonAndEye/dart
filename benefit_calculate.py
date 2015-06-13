10fd268226f88a8b166aa1494e782b929c304f15
MoonAndEye <atimis19@gmail.com>
2015-06-14 00:31:04 +0900

    Finish the benefit_array
    明天把結算算出來，並加總

--- benefit_calculate.py
+++ benefit_calculate.py
@@ -5,6 +5,7 @@ Created on Sat Jun 13 16:28:00 2015
 """
 
 import os
+import numpy
 
 def csvToArray(day_array):
     def_array = []
@@ -33,12 +34,39 @@ for i in range(1,6):
 #未來一定要找一下為什麼，可能問題在 飛標原始檔 ，不然就是寫入list那邊
 dart_d1.append(str(dart_name_list[dart_target]))
 #加入日期在 dart_d1[5]，如果以後要debug，可以直接呼叫
-"""
-for i in range(1,6):
-    print (dart_d1[i])
-"""
+
+for i in range(0,5):
+    print (dart_d1[i][0])
+
+history_list = []
+
+for name in os.listdir('C:/1save/jpStock/raw'):
+    history_list.append(name)
+
+#以下是把d0和歷史資料的位置抓出來，預測是d1，所以算損益要  d1 = d0 +1
+d0index = history_list.index(str(dart_name_list[dart_target])[:-4] + '.csv')
+print (d0index)
+d1index = int(d0index + 1)
+print (d1index)
+print (history_list[d1index]) #這就是目標
+
+d1_array = csvToArray('C:/1save/jpStock/raw/' + history_list[d1index])
+#d1_array 是歷史資料的array
+
+benefit_array = []
+a = int(len(d1_array))
+print (a)
 
 
+
+for code in range(a):
+    if d1_array[code][0] == '4558-T':
+        print (code)
+        benefit_array.append(d1_array[code])    
+
+print (benefit_array)
+
+    
 """
 with open ('C:/1save/jpStock/dart/detail/detail.txt', 'at', encoding = 'shift-jis') as f:
     f.write ('test')
