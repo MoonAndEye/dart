334dbd321ff68c40973c465f278ce43777d70855
MoonAndEye <atimis19@gmail.com>
2015-06-13 12:20:46 +0900

    加上日期變數

--- shoot_dart.py
+++ shoot_dart.py
@@ -12,9 +12,6 @@ history_list = []
 # d0 表示的是當天的股價, d1是前1天,d2是前2天
 d0_array = []
 d1_array = []
-d2_array = []
-d3_array = []
-d4_array = []
 
 def csvToArray(day_array):
     def_array = []
@@ -38,25 +35,30 @@ d3 = history_number -4
 d4 = history_number -5
 
 
-print (history_list[2])
-print (history_list[3])
-print (history_list[4])
+print (history_list[d0])
+print (history_list[d1])
+#print (history_list[4])
+date_d0 = history_list[d0][:-4]
+date_d1 = history_list[d1][:-4]
+print (date_d0)
+print (date_d1)
+
 
 d0_array = csvToArray('C:/1save/jpStock/raw/' + history_list[d0])
+d1_array = csvToArray('C:/1save/jpStock/raw/' + history_list[d1])
 #print (d0_array[2])
 
 dart_array = []
 
 for i in range(5): 
-    dart1 = random.choice(d0_array)
-    dart_array.append(dart1)
-    #print (dart1)
+    dart0 = random.choice(d0_array)
+    dart_array.append(dart0)
+    #print (dart0[0])
 
-with open('C:/1save/jpStock/dart/test.csv', 'xt', encoding = 'shift-jis') as f:
-    for i in range(5):
-        f.write(str(dart_array[i]).replace('\'', '')[1:-1] + '\n\r')
-        #f.write (str(dart_array[i]).strip("'")[1:-1]+ '\n\r')
-        #f.write(str(dart_array[i]))
+#with open('C:/1save/jpStock/dart/test.csv', 'xt', encoding = 'shift-jis') as f:
+#    for i in range(5):
+#        f.write(str(dart_array[i]).replace('\'', '')[1:-1] + '\n\r')
+        
 #for i in range(0,5):
 #    print ("d" + str(i) +"=" +  str(random.randint(0,2000))
 
