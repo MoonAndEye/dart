f7b13f2b832c53a16163c6ef5f006e8f13e9ff40
Moonandeye <MoonAndEye@users.noreply.github.com>
2016-03-04 15:46:28 +0900

    做出骰子功能
    骰子只有兩個結果
    keep
    sell

--- transfer.py
+++ transfer.py
@@ -5,6 +5,7 @@ Created on Thu Mar  3 12:45:38 2016
 @author: g3094510
 """
 import os
+import random
 
 def csvToArray(day_array, encode_setting):
     def_array = []
@@ -23,3 +24,24 @@ def sortDataFromNew (source_list, index):
         def_diction[code + str(i)] = source_list[history_number]
         history_number = history_number - 1
     return def_diction
+    
+simulation = ['0000-T', '1111-T','2222-T','3333-T','4444-T',]    
+judgement_dice = ['sell', 'keep']
+
+#dice_result = random.choice(judement_dice)
+
+def throw_dice(input_list,dice):
+    output = []
+    
+    for each in input_list:
+        result = random.choice(dice)
+        pre_result = []
+        pre_result.append(each)
+        pre_result.append(result)
+        output.append(pre_result)
+    return output
+#print(dice_result)
+
+a = throw_dice(simulation,judgement_dice)
+
+print (a)
